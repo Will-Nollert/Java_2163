66fff21f98176df9e490ddf9bd9824a9584a2843
William Nollert <will.nollert@gmail.com>
2023-03-24 20:01:29 -0500

    add: week 2 start

---
+++ 02_week/homework/WilliamNollert.java
@@ -0,0 +1,13 @@
+/* name: william nollert 
+ * File: WilliamNollert.java
+ * Description: a program that reads some positive integer n and prints out a table with the Gregory series approximations
+πi and their distance (error) from the real π,
+*/
+public class WilliamNollert {
+    public static void main(String[] args) {
+        //ask the user for a positive integer
+        System.out.println("Enter a positive integer: ");
+        //store the user input in a variable called userInt
+    
+    }
+}
